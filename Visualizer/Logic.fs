75faf91ca5b513c1e7f453bbf10bd20f0915bb1a
Slava <sh-e-95@mail.ru>
2021-08-24 00:41:05 +0500

    Add logic

---
+++ Visualizer/Logic.fs
@@ -0,0 +1,33 @@
+﻿namespace VisualizerLogic
+
+open System
+open System.IO
+
+type IDrawer =
+    abstract PrintError: message: Exception -> unit
+    abstract PrintMessage: message: string -> unit
+    abstract GetText:message: unit -> string
+    abstract Clear:clear: unit -> unit
+
+type ProjectFile(drawer: IDrawer ) = class
+    let drawer = drawer   
+    member this.getPath =   
+        let mutable path = ""
+        let mutable havePath = false
+        while not havePath do
+            try
+                drawer.Clear()
+                drawer.PrintMessage "Please Enter File"
+                let file = drawer.GetText()
+                if File.Exists file then
+                    havePath <- true
+                    path <- file.ToString()
+                    drawer.PrintMessage "File found"
+                else
+                    failwith "File not found"
+             with
+             | :? Exception as ex -> drawer.PrintError ex
+        path
+end
+
+
--- Visualizer/Program.fs
+++ Visualizer/Program.fs
@@ -1,8 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open VisualizerLogic
+open System.Threading
+
+type Printer() = 
+    interface IDrawer with
+       member this.Clear() = 
+        Thread.Sleep(200) 
+        Console.Clear()
+       member this.GetText() = Console.ReadLine()
+       member this.PrintMessage(message: string) = printfn "%s" message
+       member this.PrintError(ex: Exception) = printfn "%s" ex.Message
+
+let cells isLine size = 
+    let point = "*"   
+    let empty = " "
+    let mutable line = ""
+    if isLine then
+        for i in  1..size do 
+            line <- line + point
+    else
+        line <- line + point
+        for i in  2..size - 1 do
+            line <- line + empty
+        line <- line + point
+    line
+
+let printFrame height weight = 
+    Console.Clear()
+    for y in 0..height do
+        Console.WriteLine(cells true weight);
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main argv =   
+    let file = new ProjectFile(new Printer())
+    printf "%s" file.getPath
+    0
+ 
